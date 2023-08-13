22ccaa3501e828d5cefc21469f7dd5b85014df74
Leonard <leonardvasquez22@gmail.com>
2023-08-13 10:06:51 -0500

    update data cards

--- src/data/data-cards.js
+++ src/data/data-cards.js
@@ -3,31 +3,31 @@ export const dataCards = [
   {
     id: 1,
     url: "https://assets.adidas.com/images/w_600,f_auto,q_auto/f9df2ee87f4f46ca92e5ae58010628a2_9366/Camiseta_de_Local_Junior_FC_Rojo_GB1707_01_laydown.jpg",
-    titulo: "Camiseta de Local",
+    titulo: "Camiseta Junior de Barranquilla",
     descripcion: "La camiseta de local del Junior de Barranquilla es una mezcla perfecta de pasión, tradición y elegancia."
   },
   {
     id: 2,
-    url: "https://assets.adidas.com/images/w_600,f_auto,q_auto/f9df2ee87f4f46ca92e5ae58010628a2_9366/Camiseta_de_Local_Junior_FC_Rojo_GB1707_01_laydown.jpg",
-    titulo: "Camiseta de Local",
+    url: "https://assets-es.imgfoot.com/media/cache/800x800/rm-home-2023-2024.jpg",
+    titulo: "Camiseta Real Madrid",
     descripcion: "La camiseta de local del Junior de Barranquilla es una mezcla perfecta de pasión, tradición y elegancia."
   },
   {
     id: 3,
-    url: "https://assets.adidas.com/images/w_600,f_auto,q_auto/f9df2ee87f4f46ca92e5ae58010628a2_9366/Camiseta_de_Local_Junior_FC_Rojo_GB1707_01_laydown.jpg",
-    titulo: "Camiseta de Local",
+    url: "https://mirodeportes.com/wp-content/uploads/2018/12/adidas_colombia_amarilla_loca.jpg",
+    titulo: "Camiseta Selección Colombia",
     descripcion: "La camiseta de local del Junior de Barranquilla es una mezcla perfecta de pasión, tradición y elegancia."
   },
   {
     id: 4,
-    url: "https://assets.adidas.com/images/w_600,f_auto,q_auto/f9df2ee87f4f46ca92e5ae58010628a2_9366/Camiseta_de_Local_Junior_FC_Rojo_GB1707_01_laydown.jpg",
-    titulo: "Camiseta de Local",
+    url: "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/283/956/products/hbbxqlvbbtpodqm_cleanup1-417a3efc67ec881b9e16729532940400-640-0.webp",
+    titulo: "Camiseta Al-Nassr",
     descripcion: "La camiseta de local del Junior de Barranquilla es una mezcla perfecta de pasión, tradición y elegancia."
   },
   {
     id: 5,
-    url: "https://assets.adidas.com/images/w_600,f_auto,q_auto/f9df2ee87f4f46ca92e5ae58010628a2_9366/Camiseta_de_Local_Junior_FC_Rojo_GB1707_01_laydown.jpg",
-    titulo: "Camiseta de Local",
+    url: "https://down-co.img.susercontent.com/file/sg-11134201-7qvg5-lganps58t6khbb",
+    titulo: "Camiseta Manchester United",
     descripcion: "La camiseta de local del Junior de Barranquilla es una mezcla perfecta de pasión, tradición y elegancia."
   }
 ]
